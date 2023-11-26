e12537c7f48f4293f5fa3f7621113b3d528b83c9
Abdulsalam Abdikarim <achalam00@gmail.com>
2023-11-26 23:53:59 +0300

    3 solved

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,49 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    food_names = list()
+    for food in spicy_foods:
+        food_names.append(food["name"])
+    return food_names
+
+print(get_names(spicy_foods))
+
+# def get_spiciest_foods(spicy_foods):
+#     spiciest_foods = list()
+#     for food in spicy_foods:
+#         if food["heat_level"] > 5:
+#             spiciest_foods.append(food)
+#     return spiciest_foods
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = [food for food in spicy_foods if food["heat_level"] > 5]
+    return spiciest_foods
+
+print(get_spiciest_foods(spicy_foods))
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: 🌶 *{food['heat_level']} )")
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+
+print(get_spicy_food_by_cuisine(spicy_foods, "Thai"))
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in spicy_foods:   
+        if food["heat_level"] > 5:
+            print(f"{food['name']} ({food['cuisine']}) | Heat Level: (🌶) *{food['heat_level']} )")
+
+print(print_spiciest_foods(spicy_foods))
 
 def get_average_heat_level(spicy_foods):
-    pass
+    for food in spicy_foods:
+        pass
 
 def create_spicy_food(spicy_foods, spicy_food):
     pass
